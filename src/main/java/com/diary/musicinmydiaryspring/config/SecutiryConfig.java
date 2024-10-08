b98d23ff0c28a875f9bcf06624e522fc5a3d7c6b
cse <choisui@gmail.com>
2024-10-08 14:49:01 +0900

    feat: 회원 관리 기능 구현 및 보안 설정 추가

---
+++ src/main/java/com/diary/musicinmydiaryspring/config/SecutiryConfig.java
@@ -0,0 +1,74 @@
+package com.diary.musicinmydiaryspring.config;
+
+import com.diary.musicinmydiaryspring.jwt.service.JwtService;
+import com.diary.musicinmydiaryspring.auth.filter.LoginFilter;
+import com.diary.musicinmydiaryspring.jwt.filter.JwtAuthenticationFilter;
+import com.diary.musicinmydiaryspring.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecutiryConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtService jwtService;
+    private final MemberService memberService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws
+            Exception{
+            return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(((auth)-> auth.disable()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(getWhiteListUris()).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterAt(
+                        new LoginFilter(authenticationManager(authenticationConfiguration), jwtService, memberService),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws
+            Exception{
+            return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    private String[] getWhiteListUris() {
+        return new String[]{"/swagger-ui/**", "/v3/**", "/login", "/swagger-ui.html", "/swagger-resources/**", "/signup"};
+    }
+}
