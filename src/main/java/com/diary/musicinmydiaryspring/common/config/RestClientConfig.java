cae535b1a7ede16f71d91bfc439b8df7bfc9cd61
cse <choisui@gmail.com>
2024-10-28 21:03:15 +0900

    fix: web flux 대신 proxy 사용하여 rest client 구현

--- src/main/java/com/diary/musicinmydiaryspring/common/config/RestClientConfig.java
+++ src/main/java/com/diary/musicinmydiaryspring/common/config/RestClientConfig.java
@@ -1,14 +1,46 @@
 package com.diary.musicinmydiaryspring.common.config;
 
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
 
+    private static final int READ_TIMEOUT = 1500;
+    private static final int CONNECT_TIMEOUT = 3000;
+
     @Bean
-    public RestClient restClient(){
-        return RestClient.create();
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setHttpClient(createHttpClient());
+        return new RestTemplate(factory);
+    }
+
+    // HttpClient 생성 메서드
+    private CloseableHttpClient createHttpClient() {
+        // 요청 설정에 대한 타임아웃 값 설정
+        RequestConfig config = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofMilliseconds(CONNECT_TIMEOUT))
+                .setResponseTimeout(Timeout.ofMilliseconds(READ_TIMEOUT))
+                .build();
+
+        // HttpClient 생성 및 설정
+        return HttpClients.custom()
+                .setDefaultRequestConfig(config)
+                .setConnectionManager(new PoolingHttpClientConnectionManager())
+                .build();
+    }
+
+    @Bean
+    public RestClient restClient(RestTemplate restTemplate) {
+        return RestClient.create(restTemplate);
     }
 }
